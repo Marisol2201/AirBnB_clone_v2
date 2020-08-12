831ed2e0f72652fe9927d06a7afb6d9de00e2a8a
Marisol2201 <1529@holbertonschool.com>
2020-08-12 22:11:04 +0000

    itit method changed and working with kwars

--- models/base_model.py
+++ models/base_model.py
@@ -18,20 +18,26 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
+        if kwargs is not None and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if '__class__' != key:
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value,
-                                                  '%Y-%m-%dT%H:%M:%S.%f')
+                if key == "__class__":
+                    pass
+                elif key == "created_at":
+                    self.created_at = (datetime.strptime(
+                                       (kwargs["created_at"]),
+                                       "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key == "updated_at":
+                    self.updated_at = (datetime.strptime(
+                                       (kwargs["updated_at"]),
+                                       "%Y-%m-%dT%H:%M:%S.%f"))
+                else:
                     setattr(self, key, value)
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.today()
+            self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance"""
