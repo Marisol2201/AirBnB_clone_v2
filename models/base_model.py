60635db9315e036f4c96772efe82acd834542a03
CrisRuedaP <crisruedap@gmail.com>
2020-08-13 21:35:18 +0000

    fixed base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -21,22 +21,22 @@ class BaseModel:
         if len(kwargs) == 0:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
-        if "created_at" in kwargs:
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
         else:
-            self.created_at = datetime.now()
-        if "updated_at" in kwargs:
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"],
-                                                     "%Y-%m-%dT%H:%M:%S.%f")
-        else:
-            self.updated_at = datetime.now()
-        if "id" not in kwargs:
-            self.id = str(uuid.uuid4())
-        for key, value in kwargs.items():
-            if "__class__" not in key:
-                setattr(self, key, value)
+            if "created_at" in kwargs:
+                kwargs["created_at"] = datetime.strptime(
+                    kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.created_at = datetime.now()
+            if "updated_at" in kwargs:
+                kwargs["updated_at"] = datetime.strptime(
+                    kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                self.updated_at = datetime.now()
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            for key, value in kwargs.items():
+                if "__class__" not in key:
+                    setattr(self, key, value)
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -52,8 +52,8 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = dict(self.__dict__)
-        dictionary["__class__"] = self.__class__.__name__
+        dictionary = self.__dict__.copy()
+        dictionary["__class__"] = type(self).__name__
         dictionary["created_at"] = self.created_at.isoformat()
         dictionary["updated_at"] = self.updated_at.isoformat()
         if "_sa_instance_state" in dictionary.keys():
